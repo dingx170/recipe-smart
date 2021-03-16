e5eac1c3fc2f005bf5da54ab12bbdd962643fac5
Ruoyang Qiu <robinqiu1992@gmail.com>
2021-03-16 00:39:51 -0700

    extended cookie age to 1h

--- out/App.js
+++ out/App.js
@@ -55,7 +55,7 @@ class App {
             saveUninitialized: false,
             resave: false,
             cookie: {
-                maxAge: 10 * 1000 * 6 // expiration time in mili
+                maxAge: 60 * 1000 * 60 // expiration time in mili
             }
         }));
         // this.expApp.use(cookieParser());
