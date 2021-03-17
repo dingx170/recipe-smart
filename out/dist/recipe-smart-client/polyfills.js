2ab13249615fbd30ee410393712ec3da884a0478
tongding <dingx170@gmail.com>
2021-03-17 12:14:59 -0700

    replace client files

--- out/dist/recipe-smart-client/polyfills.js
+++ out/dist/recipe-smart-client/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/haoli/SAAS/recipe-smart-client/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\dinto\Code\SAAS\recipe-smart-client\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
