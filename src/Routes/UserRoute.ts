df40d70872336e01579eba81b517c0f5c7b719b3
roadmonster <roadmonster1989@gmail.com>
2021-03-17 14:32:01 -0700

    removed junk code and logging code

--- src/Routes/UserRoute.ts
+++ src/Routes/UserRoute.ts
@@ -8,16 +8,7 @@ class UserRoute{
     public static registerRoutes(userRoute: Router) {
 
         userRoute.get("/api/user/:id", UserController.retrieveUserByID);
-        
-        userRoute.post("/api/user", UserController.createUser);
-
         userRoute.put("/api/user/:id", UserController.updateUser);
-
-        userRoute.post("/api/login", UserController.logincheck);
-
-        userRoute.get("/api/login", UserController.getSession);
-
-        userRoute.get("/api/validation", UserController.validateNameEmail);
     }
 
 
